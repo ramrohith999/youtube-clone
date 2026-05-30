c033ea802b62c86bd36828d91a16b38809c32bf7
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-05-30 16:55:07 +0530

    fixed errors

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 //connected to mongodb with mongoose
 const connectDB= async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("momgodb connected");
     }catch(error){
         console.log(error.message);
