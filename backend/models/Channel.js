fb2639e7c2a84c099c757e06de16d3c9ad934d3d
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-05-30 16:38:31 +0530

    added all the models

---
+++ backend/models/Channel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+  {
+    channelName: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+
+    banner: {
+      type: String,
+      default: "",
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Channel", channelSchema);
---
+++ backend/models/Video.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    uploader: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Channel",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Video", videoSchema);
