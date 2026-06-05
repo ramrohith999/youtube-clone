c263f51c10e55f63b11e85d66020793a22badda5
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-06-05 14:53:57 +0530

    adde like and dislike buttons on ui and they are working

--- frontend/src/pages/VideoPlayer.jsx
+++ frontend/src/pages/VideoPlayer.jsx
@@ -1,15 +1,19 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
 
 import MainLayout from "../layouts/MainLayout";
 
 import { fetchVideoById } from "../features/videos/videoSlice";
 
 import getYoutubeEmbedUrl from "../utils/getYoutubeEmbedUrl";
+import { likeVideo, dislikeVideo } from "../services/videoService";
 
 const VideoPlayer = () => {
   const { id } = useParams();
+
+  const userId = "6a1b11a1f2b72e71b85a7a73";
 
   const dispatch = useDispatch();
 
@@ -35,6 +39,18 @@ const VideoPlayer = () => {
     );
   }
 
+  const handleLike = async () => {
+    await likeVideo(id, userId);
+
+    dispatch(fetchVideoById(id));
+  };
+
+  const handleDislike = async () => {
+    await dislikeVideo(id, userId);
+
+    dispatch(fetchVideoById(id));
+  };
+
   return (
     <MainLayout>
       <div className="max-w-5xl">
@@ -51,6 +67,42 @@ const VideoPlayer = () => {
         </p>
 
         <p className="text-gray-500 mt-2">{currentVideo.views} views</p>
+
+        <div className="flex gap-4 mt-4">
+          <button
+            onClick={handleLike}
+            className="
+      flex
+      items-center
+      gap-2
+      bg-gray-100
+      px-4
+      py-2
+      rounded-full
+    "
+          >
+            <FaThumbsUp />
+
+            {currentVideo.likes.length}
+          </button>
+
+          <button
+            onClick={handleDislike}
+            className="
+      flex
+      items-center
+      gap-2
+      bg-gray-100
+      px-4
+      py-2
+      rounded-full
+    "
+          >
+            <FaThumbsDown />
+
+            {currentVideo.dislikes.length}
+          </button>
+        </div>
 
         <div className="bg-gray-100 p-4 rounded-xl mt-4">
           <p>{currentVideo.description}</p>
--- frontend/src/services/videoService.js
+++ frontend/src/services/videoService.js
@@ -9,3 +9,27 @@ export const getVideoById = async (id) => {
   const response = await API.get(`/videos/${id}`);
   return response.data;
 };
+
+export const likeVideo = async (
+  videoId,
+  userId
+) => {
+  const response = await API.patch(
+    `/videos/${videoId}/like`,
+    { userId }
+  );
+
+  return response.data;
+};
+
+export const dislikeVideo = async (
+  videoId,
+  userId
+) => {
+  const response = await API.patch(
+    `/videos/${videoId}/dislike`,
+    { userId }
+  );
+
+  return response.data;
+};
