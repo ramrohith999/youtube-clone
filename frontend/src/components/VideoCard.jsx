ff2fdce905831eddb4035a54c742d75421f40eed
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-06-06 16:12:47 +0530

    fixed ui for videocard

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,26 +1,60 @@
 import { Link } from "react-router-dom";
 
+//video card component
 const VideoCard = ({ video }) => {
   return (
+    //video and its link
     <Link to={`/video/${video._id}`} className="block">
-      <div>
+      <div
+        className="
+          bg-white
+          rounded-xl
+          overflow-hidden
+          shadow-sm
+          hover:shadow-xl
+          hover:-translate-y-1
+          transition-all
+          duration-300
+        "
+      >
+        {/*thumbnail*/}
         <img
           src={video.thumbnailUrl}
           alt={video.title}
-          className="w-full rounded-xl"
+          className="
+            w-full
+            h-48
+            object-cover
+          "
         />
 
-        <div className="mt-2">
-          <h3 className="font-semibold">{video.title}</h3>
+        <div className="p-3">
+          <h3
+            className="
+              font-semibold
+              line-clamp-2
+              min-h-12
+            "
+          >
+            {video.title}
+          </h3>
 
           <Link
             to={`/channel/${video.channel?._id}`}
-            className=" text-gray-600 text-sm hover:text-blue-500"
+            className="
+              text-gray-800
+              text-sm
+              hover:text-blue-600
+              transition
+              duration-200
+            "
           >
             {video.channel?.channelName}
           </Link>
 
-          <p className="text-gray-500 text-sm">{video.views} views</p>
+          <p className="text-gray-500 text-sm mt-1">
+            {video.views?.toLocaleString()} views
+          </p>
         </div>
       </div>
     </Link>
